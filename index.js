b013915d8edd14e215b9fdedd54473a68966a36e
JonahPeck <PeckJonahC@gmail.com>
2023-02-06 11:39:07 -0700

    completed

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your code here!
+// document.body.append("h1");
+// main.remove();
+// const newHeader = document.createElement("h1");
+// document.body.append(newHeader);
+// document.getElementById(newHeader).id = "victory";
+
+// const element = document.createElement("div");
+// document.body.append(element);
+
+// const ul = document.createElement("ul");
+// for (let i = 0; 1 < 3; i++) {
+//     const li = document.createElement("li");
+//     li.textContent = (i+1).toString();
+//     ul.append(li);
+// }
+main.remove();
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "Man U is the champion!";
+newHeader.className = "victory";
